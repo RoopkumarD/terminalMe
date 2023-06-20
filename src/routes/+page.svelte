060a3b8f613d334a5f2ac8068bf2b92c4d9b86d2
Roopkumar Das <dasroopkumar5@gmail.com>
2023-06-21 00:11:25 +0530

    focus input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+
   let command = "";
+  let cli: HTMLInputElement;
+
   $: if (command[command.length - 1] === " ") {
     command = command.slice(0, command.length - 1);
     command = command + "\u00A0";
   }
+
+  onMount(() => {
+    cli.focus();
+  });
 </script>
 
 <div id="container" class="min-h-full flex justify-center items-center">
   <div
     id="terminal"
     class="bg-zinc-950 h-96 w-1/2 font-jetBrain text-white text-xl p-4"
+    on:click={() => {
+      cli.focus();
+    }}
   >
     <div id="content">
       <p><span class="text-amber-400">&gt;</span> Hello Ji mera naam roop</p>
     </div>
-    <div id="command" class="">
+
+    <div id="command">
       <p class="break-all">
         <span class="m-0 p-0 text-amber-400">&gt;&nbsp;</span>{command}<input
           bind:value={command}
+          bind:this={cli}
           type="text"
-          class="m-0 p-0 outline-none z-50 w-2 h-min bg-white text-white"
+          class="m-0 p-0 z-50 w-2 h-min bg-white text-white"
         />
       </p>
     </div>
