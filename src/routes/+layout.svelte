b66a16b6cb2eb6f8da262b284983c98fae561757
Roopkumar Das <dasroopkumar5@gmail.com>
2023-06-20 22:01:55 +0530

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../app.css";
+</script>
+
+<slot />
+
+<style lang="postcss">
+  :global(html) {
+    background-color: #f1f5f9;
+  }
+
+  :global(body) {
+    height: 100vh;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let command = "";
+  $: if (command[command.length - 1] === " ") {
+    command.replace(" ", "&nbsp;");
+    console.log(command);
+  }
+</script>
+
+<div id="container" class="min-h-full flex justify-center items-center">
+  <div
+    id="terminal"
+    class="bg-zinc-950 h-96 w-1/2 font-jetBrain text-white text-xl p-4"
+  >
+    <div id="content">
+      <p><span class="text-amber-400">&gt;</span> Hello Ji mera naam roop</p>
+    </div>
+    <div id="command" class="">
+      <div id="cli" class="">
+        <p class="break-all">
+          <span class="m-0 p-0 text-amber-400">&gt;&nbsp;</span>{command}<input
+            bind:value={command}
+            type="text"
+            class="m-0 p-0 outline-none z-50 w-2 h-min bg-white text-white"
+          />
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
