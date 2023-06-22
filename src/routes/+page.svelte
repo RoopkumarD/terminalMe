ff85f1b1c3bd01695d1bf4cc0c86182e88dc8f27
Roopkumar Das <dasroopkumar5@gmail.com>
2023-06-23 00:54:11 +0530

    change way to add text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,16 @@
   import { onMount } from "svelte";
 
   let command = "";
-  let cli: HTMLInputElement;
 
   let show = false;
-  let isTextSelected = false;
 
   $: if (command[command.length - 1] === " ") {
     command = command.slice(0, command.length - 1);
     command = command + "\u00A0";
+  }
+
+  function handleKeydown(event: KeyboardEvent) {
+    command += event.key;
   }
 
   onMount(() => {
@@ -17,21 +19,12 @@
   });
 </script>
 
+<svelte:window on:keydown={handleKeydown} />
+
 <div id="container" class="min-h-full flex justify-center items-center">
   <div
     id="terminal"
     class="bg-zinc-950 h-96 w-1/2 overflow-y-auto font-jetBrain text-white text-xl p-4"
-    on:click={() => {
-      if (!isTextSelected) {
-        cli.focus();
-      }
-    }}
-    on:mouseup={() => {
-      isTextSelected = window.getSelection().toString() !== "";
-    }}
-    on:keydown={() => {
-      cli.focus();
-    }}
   >
     {#if show}
       <div id="content">
@@ -40,15 +33,11 @@
         </p>
       </div>
 
-      <div id="command">
+      <div id="command" class="">
         <p class="break-all">
-          <!--   <span class="m-0 p-0 text-amber-400">&gt;&nbsp;</span>{command}<input -->
-          <!--     bind:value={command} -->
-          <!--     bind:this={cli} -->
-          <!--     type="text" -->
-          <!--     class="m-0 p-0 outline-none z-50 w-2 h-min bg-white text-white" -->
-          <!--   /> -->
-          <!-- </p> -->
+          <span class="m-0 p-0 text-amber-400">&gt;&nbsp;</span>{command}<span
+            class="w-2 h-6 bg-white inline-block"
+          />
         </p>
       </div>
     {:else if !show}
