ea9417c14000ac0cd26b7e36328350e3aa1e06f6
Roopkumar Das <dasroopkumar5@gmail.com>
2023-06-28 00:51:32 +0530

    added ls command

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,19 +3,112 @@
 
   let command = "";
   let cli: HTMLInputElement;
+  let content: HTMLDivElement;
 
   let show = false;
+
+  let values = [
+    {
+      name: "Me",
+      values: "I am powerful",
+      type: "directory",
+      folder: [
+        {
+          name: "Inside Me",
+          values: "Don't be dirty minded",
+          type: "file",
+        },
+      ],
+    },
+    {
+      name: "Projects",
+      values: "Firefox extension",
+      type: "directory",
+    },
+    {
+      name: "Hello JI mera naam roop",
+      values: "timepass",
+      type: "directory",
+    },
+  ];
+  let currentDirectory = values;
+
+  function addCommand() {
+    let p = document.createElement("p");
+    let span = document.createElement("span");
+    span.textContent = "> ";
+    span.classList.add("text-amber-400");
+    p.classList.add("whitespace-break-spaces");
+    p.classList.add("break-all");
+    p.textContent = command;
+    p.insertBefore(span, p.firstChild);
+    content.appendChild(p);
+  }
+
+  function spaceInString(str: string) {
+    for (let i of str) {
+      if (i === " ") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function lsCommand() {
+    let p = document.createElement("p");
+    p.classList.add("text-orange-500");
+    p.classList.add("whitespace-break-spaces");
+    p.classList.add("break-all");
+    for (let element of values) {
+      let span = document.createElement("span");
+      let check = spaceInString(element.name);
+      if (check === true) {
+        span.textContent = "'" + element.name + "'" + "   ";
+      } else if (check === false) {
+        span.textContent = element.name + "   ";
+      }
+      p.appendChild(span);
+    }
+    content.appendChild(p);
+  }
+
+  function dirAvailable(name: string) {
+    for (let element of currentDirectory) {
+      if (element.name === name && element.type === "directory") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function cdCommand() {
+    let check = dirAvailable(command.substring(3));
+    if (check === true) {
+      currentDirectory = command.substring(3);
+    } else if (check === false) {
+    }
+  }
+
+  function pressKey(event: KeyboardEvent) {
+    if (event.key == "Enter") {
+      addCommand();
+      if (command === "ls") {
+        lsCommand();
+      } else if (command === "cd") {
+        cdCommand();
+      }
+      command = "";
+    } else {
+      cli.focus();
+    }
+  }
 
   onMount(() => {
     show = true;
   });
 </script>
 
-<svelte:window
-  on:keydown={() => {
-    cli.focus();
-  }}
-/>
+<svelte:window on:keydown={pressKey} />
 
 <input
   type="text"
@@ -30,9 +123,9 @@
     class="bg-zinc-950 h-96 w-1/2 overflow-y-auto font-jetBrain text-white text-xl p-4"
   >
     {#if show}
-      <div id="content">
+      <div id="content" bind:this={content}>
         <p>
-          <span class="text-amber-400">&gt;</span> Hello Ji mera naam roop
+          <span class="text-amber-400">&gt;&nbsp;</span>Hello Ji mera naam roop
         </p>
       </div>
 
